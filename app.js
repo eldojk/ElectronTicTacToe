5aaadb288d61e1b6dec01e893738969dc303f020
Eldo Joseph <eldo.joseph@oracle.com>
2017-05-07 23:01:17 +0530

    Finished game logic. app done

--- app.js
+++ app.js
@@ -2,24 +2,32 @@ var app = angular.module('myApp', []);
 
 app.controller('hangmanController', function($scope) {
 
-  
+  $scope.cellPriority = [
+    [4], [0, 2, 6, 8], [1, 3, 5, 7]
+  ];
 
   $scope.reset = function() {
     $scope.board = [
-      [ { value: '-' }, { value: '-' }, { value: '-' } ],
-      [ { value: '-' }, { value: '-' }, { value: '-' } ],
-      [ { value: '-' }, { value: '-' }, { value: '-' } ]
+      [ { value: '-', class: 'btn-primary btn-xl' }, { value: '-', class: 'btn-primary btn-xl' }, { value: '-', class: 'btn-primary btn-xl' } ],
+      [ { value: '-', class: 'btn-primary btn-xl' }, { value: '-', class: 'btn-primary btn-xl' }, { value: '-', class: 'btn-primary btn-xl' } ],
+      [ { value: '-', class: 'btn-primary btn-xl' }, { value: '-', class: 'btn-primary btn-xl' }, { value: '-', class: 'btn-primary btn-xl' } ]
     ];
     
     $scope.currentPlayer = 'X';
     $scope.winner = false;
+    $scope.draw = false;
     $scope.winningPlayer = 'X';
+    $scope.gameFinished = false;
+    $scope.winningMessage = 'The house has won!';
   };
   
   $scope.reset();
-  
-  $scope.isTaken = function(cell) {
-    return cell.value !== '-';
+
+  $scope.isCellDisabled = function(cell) {
+    if (cell.value !== '-' || $scope.gameFinished)
+      return true;
+
+    return false;
   };
   
   var checkForMatch = function(cell1, cell2, cell3) {
@@ -31,10 +39,66 @@ app.controller('hangmanController', function($scope) {
   var processWinner = function (winner) {
     $scope.winner = true;
     $scope.winningPlayer = winner;
+
+    if ($scope.winningPlayer === 'X') {
+      $scope.winningMessage = 'You win. Congrats!';
+    }
   };
 
-  var markUpCells = function(rcVal, type) {
+  var finishGame = function() {
+    $scope.gameFinished = true;
+  };
 
+
+  var markUpCells = function(rcVal, type) {
+    var successClass = 'btn-success btn-xl';
+
+    if (type === 'row') {
+      var cells = $scope.board[rcVal];
+      for (var i = 0; i < 3; i++) {
+        c = cells[i];
+        c.class = successClass;
+      }
+    }
+
+    else if (type === 'col') {
+      for (var i = 0; i < 3; i++) {
+        c = $scope.board[i][rcVal];
+        c.class = successClass;
+      }
+    }
+
+    else {
+      if (rcVal === 1) {
+        $scope.board[0][0].class = successClass;
+        $scope.board[1][1].class = successClass;
+        $scope.board[2][2].class = successClass;
+      }
+
+      else if (rcVal == 2) {
+        $scope.board[0][2].class = successClass;
+        $scope.board[1][1].class = successClass;
+        $scope.board[2][0].class = successClass;
+      }
+    }
+
+    finishGame();
+  };
+
+  var processDraw = function() {
+    $scope.draw = true;
+  };
+
+  var checkForDraw = function() {
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if ($scope.board[i][j].value === '-') {
+          return false;
+        } 
+      }
+    }
+
+    return true;
   };
   
   var checkForEndOfGame = function() {
@@ -88,21 +152,191 @@ app.controller('hangmanController', function($scope) {
         markUpCells(2, 'diag')
         return true;
     }
+
+    // check for draw
+    if (checkForDraw()) {
+      processDraw();
+      finishGame();
+      return true;
+    }
     
     return false;
   };
 
-  var play = function() {
+  var getHorizontalWinnableCell = function(character) {
+    for (var row = 0; row < 3; row++) {
+      var characterCount = 0;
+      var emptyCellCount = 0;
+      var emptyCell = null;
 
+      for (var col = 0; col < 3; col++) {
+        if ($scope.board[row][col].value === character)
+          characterCount++;
+
+        else if ($scope.board[row][col].value === '-') {
+          emptyCellCount++;
+          emptyCell = $scope.board[row][col];
+        }
+      }
+
+      if (characterCount === 2 && emptyCellCount === 1)
+        return emptyCell;
+    }
+
+    return null;
+  };
+
+  var getVerticalWinnableCell = function(character) {
+    for (var col = 0; col < 3; col++) {
+      var characterCount = 0;
+      var emptyCellCount = 0;
+      var emptyCell = null;
+
+      for (var row = 0; row < 3; row++) {
+        if ($scope.board[row][col].value === character)
+          characterCount++;
+
+        else if ($scope.board[row][col].value === '-') {
+          emptyCellCount++;
+          emptyCell = $scope.board[row][col];
+        }
+      }
+
+      if (characterCount === 2 && emptyCellCount === 1)
+        return emptyCell;
+    }
+
+    return null;
+  };
+
+  var getWinnableCellInForwardDiagonal = function(character) {
+    var characterCount = 0;
+    var emptyCellCount = 0;
+    var emptyCell = null;
+
+    for (var i = 0; i < 3; i++) {
+      if ($scope.board[i][i].value === character)
+        characterCount++;
+
+      else if ($scope.board[i][i].value === '-') {
+        emptyCellCount++;
+        emptyCell = $scope.board[i][i];
+      }
+    }
+
+    if (characterCount === 2 && emptyCell === 1)
+      return emptyCell;
+
+    return null;
+  };
+
+  var getWinnableCellInBackwardDiagonal = function(character) {
+    var characterCount = 0;
+    var emptyCellCount = 0;
+    var emptyCell = null;
+
+    for (var i = 0; i < 3; i++) {
+      var j = 3 - i - 1;
+      if ($scope.board[i][j].value === character)
+        characterCount++;
+
+      else if ($scope.board[i][j].value === '-') {
+        emptyCellCount++;
+        emptyCell = $scope.board[i][j];
+      }
+    }
+
+    if (characterCount === 2 && emptyCell === 1)
+      return emptyCell;
+
+    return null;
+  };
+
+  var getDiagonalWinnableCell = function(character) {
+    var winnableCell = getWinnableCellInForwardDiagonal(character);
+
+    if (winnableCell !== null)
+      return winnableCell;
+
+    return getWinnableCellInBackwardDiagonal(character);
+  };
+
+  var getWinnableCell = function(character) {
+    var winnableCell = getHorizontalWinnableCell(character);
+
+    if (winnableCell !== null)
+      return winnableCell;
+
+    winnableCell = getVerticalWinnableCell(character);
+
+    if (winnableCell !== null) 
+      return winnableCell;
+
+    return getDiagonalWinnableCell(character);
+  };
+
+  var isCellFreeToPlay = function(cellId) {
+    var i = 0;
+
+    while (cellId >= 3) {
+      cellId = cellId - 3;
+      i++;
+    }
+
+    var j = cellId;
+
+    if ($scope.board[i][j].value === '-')
+      return $scope.board[i][j];
+
+    return null;
+  };
+
+  var getHighestPriorityFreeCell = function() {
+    for (var i in $scope.cellPriority) {
+      var pArray = $scope.cellPriority[i];
+      for (var j in pArray) {
+        var cellId = pArray[j];
+        var freeCell = isCellFreeToPlay(cellId);
+        if (freeCell !== null)
+          return freeCell;
+      }
+    }
+
+    return null;
+  };
+
+  var getCellToPlay = function() {
+    var winnableCell = getWinnableCell('O');
+
+    if (winnableCell !== null)
+      return winnableCell;
+
+    winnableCell = getWinnableCell('X');
+
+    if (winnableCell != null)
+      return winnableCell;
+
+    return getHighestPriorityFreeCell();
+  };
+
+  var switchPlayer = function() {
+    $scope.currentPlayer = $scope.currentPlayer === 'X' ? 'O' : 'X';
+  };
+
+  var play = function() {
+    var cell = getCellToPlay();
+    cell.value = 'O';
+    switchPlayer();
   };
   
   $scope.mark = function(cell) {
     cell.value = $scope.currentPlayer;
     if (!checkForEndOfGame()) {
-      $scope.currentPlayer = $scope.currentPlayer === 'X' ? 'O' : 'X';
+      switchPlayer();
 
-      if ($scope.currentPlayer == '0') {
+      if ($scope.currentPlayer === 'O') {
         play();
+        checkForEndOfGame();
       }
     }
   };
--- main.js
+++ main.js
@@ -18,7 +18,13 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 700});
+  mainWindow = new BrowserWindow(
+    {
+      width: 1000, 
+      height: 700,
+      title: 'ElectronTicTacToe'
+    }
+    );
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
